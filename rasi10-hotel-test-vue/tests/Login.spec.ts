3dfeeef52b7063107228b7569750349868de0cd3
Kriksy <viktoria.toldi@gmail.com>
2024-09-15 22:30:10 +0200

    Update login.spec.ts using LoginPage and test dashboard

--- rasi10-hotel-test-vue/tests/Login.spec.ts
+++ rasi10-hotel-test-vue/tests/Login.spec.ts
@@ -1,10 +1,41 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Test suite 01", () => {
-  test("has title", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
+import { LoginPage } from "./pages/login.page";
+import { DashboardPage } from "./pages/dashboard.page";
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+require("dotenv").config();
+
+test.describe("Test suite: Login", () => {
+  test("Login and Check Dashboard", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.performLogin(
+      `${process.env.TEST_USERNAME}`,
+      `${process.env.TEST_PASSWORD}`
+    );
+
+    await page.waitForTimeout(1000); // Wait for login to succeed
+
+    await expect(
+      page.url(),
+      "Check that we are on http://localhost:3000/"
+    ).toBe("http://localhost:3000/"); //assertion
+
+    await expect(
+      page.getByRole("heading", { name: "Tester Hotel Overview" }),
+      "Check that the heading is now Tester Hotel Overview"
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole("link", { name: "Tester Hotel" }),
+      "Check that the banner contains 'Tester Hotel'"
+    ).toBeVisible(); //assertion
+
+    //assertion
+    await expect(
+      page.getByRole("heading", { name: "Tester Hotel Overview" }),
+      "Check that dashboard contain 'Test Hotel Overview' heading"
+    ).toBeVisible();
   });
 });
