a382411172e4ce0e43afe822688b922f9b4368a4
Kriksy <viktoria.toldi@gmail.com>
2024-09-29 20:55:10 +0200

    Add EditClientsPage Page Object Model

---
+++ hotel-playwright-test-automation/pages/edit_clients.page.ts
@@ -0,0 +1,15 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+require("dotenv").config();
+
+export class EditClientsPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto(`${process.env.BASE_URL}/clients`);
+  }
+}
