99a11acdcc2bee7c2ab49e3b2f504b14161389ce
Kriksy <viktoria.toldi@gmail.com>
2024-10-06 19:07:43 +0200

    Add goto for DashboardPage

--- hotel-playwright-test-automation/pages/dashboard.page.ts
+++ hotel-playwright-test-automation/pages/dashboard.page.ts
@@ -9,15 +9,27 @@ export class DashboardPage {
   readonly viewBillsButton: Locator;
   readonly viewReservationsButton: Locator;
 
-
   constructor(page: Page) {
     this.page = page;
     this.logoutButton = page.getByRole("button", { name: "Logout" });
 
-    this.viewRoomsButton = page.locator("#app > div > div > div:nth-child(1) > a");
-    this.viewClientButton = page.locator("#app > div > div > div:nth-child(2) > a");
-    this.viewBillsButton = page.locator("#app > div > div > div:nth-child(3) > a");
-    this.viewReservationsButton = page.locator("#app > div > div > div:nth-child(4) > a");
+    this.viewRoomsButton = page.locator(
+      "#app > div > div > div:nth-child(1) > a"
+    );
+    this.viewClientButton = page.locator(
+      "#app > div > div > div:nth-child(2) > a"
+    );
+    this.viewBillsButton = page.locator(
+      "#app > div > div > div:nth-child(3) > a"
+    );
+    this.viewReservationsButton = page.locator(
+      "#app > div > div > div:nth-child(4) > a"
+    );
+  }
+
+  async goto() {
+    await this.page.goto(`/`);
+    await this.page.waitForURL(`/`);
   }
 
   async performLogout() {
