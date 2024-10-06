cda573715aa99a38a230467bcd7de501749cb9c3
Kriksy <viktoria.toldi@gmail.com>
2024-10-06 19:52:23 +0200

    Update test case Create Bill

--- hotel-playwright-test-automation/tests/bills.spec.ts
+++ hotel-playwright-test-automation/tests/bills.spec.ts
@@ -1,66 +1,60 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
-import { CreateClientsPage } from "../pages/create_clients.page";
-import { LoginPage } from "../pages/login.page";
+import { CreateBillsPage } from "../pages/create_bills.page";
+import { DashboardPage } from "../pages/dashboard.page";
 
 require("dotenv").config();
 
-test.describe("Clients", () => {
-  test("Create", async ({ page }) => {
-    const createClientsPage = new CreateClientsPage(page);
+test.describe("Bills", () => {
+  test("Create Bill", async ({ page }) => {
+    const createBillsPage = new CreateBillsPage(page);
+    const dashboardPage = new DashboardPage(page);
 
     // Browse to Dashboard page
-    await page.goto("/");
-    await page.waitForURL(`/`);
+    await dashboardPage.goto();
 
-    // Browse to Reservation page from Dashboard page
-    await page.locator("#app > div > div > div:nth-child(4) > a").click();
+    // Browse to Bills page from Dashboard page
+    await dashboardPage.viewBillsButton.click();
 
     // Check Header for page
     await expect(
-      page.getByRole("heading", { name: "Clients" }),
-      "Check that the heading is now 'Clients'"
+      page.getByRole("heading", { name: "Bills" }),
+      "Check that the heading is now 'Bills'"
     ).toBeVisible();
 
-    // Browse to Create page
-    await page.getByRole("link", { name: "Create Client" }).click();
+    // Browse to Create page using Button
+    await page.getByRole("link", { name: "Create Bill" }).click();
+
+    // Check URL
+    await page.waitForURL(`/bill/new`);
 
     // Check Header for Create page
     await expect(
-      page.getByText("New Client"),
-      "Check current page title to be New Client"
+      page.getByText("New Bill"),
+      "Check current page title to be New Bill"
     ).toBeVisible();
 
     // Create fake data
-    const fullName = faker.person.fullName();
-    const userEmail = faker.internet.email();
-    const userPhoneNo = faker.phone.number();
+    const valueSEK = faker.number.int({ min: 0, max: 1000000000 }).toString();
 
     // Fill form with fake data
     await page
-      .locator("div")
-      .filter({ hasText: /^Name$/ })
-      .getByRole("textbox")
-      .fill(fullName);
-    await page.locator('input[type="email"]').fill(userEmail);
-    await page
-      .locator("div")
-      .filter({ hasText: /^Telephone$/ })
-      .getByRole("textbox")
-      .fill(userPhoneNo);
+      //.locator("div")
+      //.filter({ hasText: /^VALUE \(SEK\)$/ })
+      .getByRole("spinbutton")
+      .fill(valueSEK);
     await page.getByText("Save").click();
 
     // Check URL after "Save" button click
+    await page.waitForURL(`/bills`);
 
     // Get last item in the list
     const element = page.locator(
-      "#app > div > div.clients > div:nth-last-child(1)"
+      "#app > div > div.bills > div:nth-last-child(1)"
     );
     // Check last item to contain the fake data
-    await expect(element).toContainText(fullName);
-    await expect(element).toContainText(userEmail);
-    await expect(element).toContainText(userPhoneNo);
+    await expect(element).toContainText(valueSEK);
 
     // Delete new entry
 
