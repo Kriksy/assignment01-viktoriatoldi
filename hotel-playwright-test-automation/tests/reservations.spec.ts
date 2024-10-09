fe4c4e79d1e3a135a80424154e649b5230912aff
Kriksy <viktoria.toldi@gmail.com>
2024-10-09 19:40:56 +0200

    Add reservations.spec.ts

---
+++ hotel-playwright-test-automation/tests/reservations.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+test.describe("Reservations", () => {
+  test("Create Reservation", async ({ page }) => {});
+});
