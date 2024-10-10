63d7a4aaf56d4404e70a1f6f3829b95171fce0ae
Kriksy <viktoria.toldi@gmail.com>
2024-10-10 12:18:21 +0200

    Add ViewBillsPage and Update Create Bill with Page Object Model

--- hotel-playwright-test-automation/tests/bills.spec.ts
+++ hotel-playwright-test-automation/tests/bills.spec.ts
@@ -1,67 +1,47 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
+import { ViewBillsPage } from "../pages/view_bills.page";
 import { CreateBillsPage } from "../pages/create_bills.page";
 import { DashboardPage } from "../pages/dashboard.page";
 
 require("dotenv").config();
 
 test.describe("Bills", () => {
+  test("View Bills", async ({ page }) => {
+    const viewBillsPage = new ViewBillsPage(page);
+    await viewBillsPage.goto();
+    await expect(page, "Check that the url is correct").toHaveURL(/.*\/bills/);
+
+    await viewBillsPage.gotoCreateBill();
+  });
+
   test("Create Bill", async ({ page }) => {
     const createBillsPage = new CreateBillsPage(page);
     const dashboardPage = new DashboardPage(page);
 
-    // Browse to Dashboard page
-    await dashboardPage.goto();
-
-    // Browse to Bills page from Dashboard page
-    await dashboardPage.viewBillsButton.click();
-
-    // Check Header for page
-    await expect(
-      page.getByRole("heading", { name: "Bills" }),
-      "Check that the heading is now 'Bills'"
-    ).toBeVisible();
-
-    // Browse to Create page using Button
-    await page.getByRole("link", { name: "Create Bill" }).click();
-
-    // Check URL
-    await page.waitForURL(`/bill/new`);
-
-    // Check Header for Create page
-    await expect(
-      page.getByText("New Bill"),
-      "Check current page title to be New Bill"
-    ).toBeVisible();
+    // Browse to Create Bill page
+    await createBillsPage.goto();
 
     // Create fake data
     const valueSEK = faker.number.int({ min: 0, max: 1000000000 }).toString();
 
     // Fill form with fake data
-    await page
-      //.locator("div")
-      //.filter({ hasText: /^VALUE \(SEK\)$/ })
-      .getByRole("spinbutton")
-      .fill(valueSEK);
-    await page.getByText("Save").click();
+    await createBillsPage.formValue.fill(valueSEK);
+    await createBillsPage.save();
 
-    // Check URL after "Save" button click
-    await page.waitForURL(`/bills`);
+    await expect(page, "Check that the url is correct").toHaveURL(/.*\/bills/);
 
+    const viewBillsPage = new ViewBillsPage(page);
+    await viewBillsPage.goto();
     // Get last item in the list
-    const element = page.locator(
-      "#app > div > div.bills > div:nth-last-child(1)"
-    );
+    const element = viewBillsPage.lastItem;
+
     // Check last item to contain the fake data
     await expect(element).toContainText(valueSEK);
 
-    // Click on "..."
-    await element.locator("div.action").click();
-
     // Delete new entry
-    const deleteElement = element.getByText("Delete");
-    await deleteElement.click();
+    await viewBillsPage.deleteLastItem();
   });
 
   test("Create Bill Error Message", async ({ page }) => {
