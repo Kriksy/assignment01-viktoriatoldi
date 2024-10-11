3f82221b2453053e70500b2f0e09d0b1c5306c93
Kriksy <viktoria.toldi@gmail.com>
2024-10-11 18:51:02 +0200

    Update test case Create Client with Page Object Model

--- hotel-playwright-test-automation/pages/create_clients.page.ts
+++ hotel-playwright-test-automation/pages/create_clients.page.ts
@@ -4,13 +4,36 @@ require("dotenv").config();
 
 export class CreateClientsPage {
   readonly page: Page;
-  readonly createClientButton: Locator;
+  readonly formName: Locator;
+  readonly formEmail: Locator;
+  readonly formPhoneNo: Locator;
+
+  readonly saveButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.formName = page
+      .locator("div")
+      .filter({ hasText: /^Name$/ })
+      .getByRole("textbox");
+    this.formEmail = page.locator('input[type="email"]');
+    this.formPhoneNo = page
+      .locator("div")
+      .filter({ hasText: /^Telephone$/ })
+      .getByRole("textbox");
+    this.saveButton = page.getByText("Save");
   }
 
   async goto() {
-    await this.page.goto(`/clients`);
+    await this.page.goto(`/client/new`);
+
+    await expect(
+      this.page.getByText("New Client"),
+      "Check current page title to be New Client"
+    ).toBeVisible();
+  }
+
+  async save() {
+    await this.saveButton.click();
   }
 }
--- hotel-playwright-test-automation/tests/clients.spec.ts
+++ hotel-playwright-test-automation/tests/clients.spec.ts
@@ -2,63 +2,39 @@ import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
 import { CreateClientsPage } from "../pages/create_clients.page";
-import { LoginPage } from "../pages/login.page";
 
-require("dotenv").config();
+import { ViewClientsPage } from "../pages/view_clients.page";
 
 test.describe("Clients", () => {
   test("Create Client", async ({ page }) => {
     const createClientsPage = new CreateClientsPage(page);
-
-    await page.goto("/");
-    await page.waitForURL(`/`);
+    const viewClientsPage = new ViewClientsPage(page);
 
     // Browse to Clients
-    await page.locator("#app > div > div > div:nth-child(2) > a").click();
-
-    await expect(
-      page.getByRole("heading", { name: "Clients" }),
-      "Check that the heading is now 'Clients'"
-    ).toBeVisible();
-
-    // Browse to Clients / Create Client
-    await page.getByRole("link", { name: "Create Client" }).click();
-
-    await expect(
-      page.getByText("New Client"),
-      "Check current page title to be New Client"
-    ).toBeVisible();
+    await createClientsPage.goto();
 
     // Create fake client data
     const fullName = faker.person.fullName();
     const userEmail = faker.internet.email();
     const userPhoneNo = faker.phone.number();
 
-    await page
-      .locator("div")
-      .filter({ hasText: /^Name$/ })
-      .getByRole("textbox")
-      .fill(fullName);
-    await page.locator('input[type="email"]').fill(userEmail);
-    await page
-      .locator("div")
-      .filter({ hasText: /^Telephone$/ })
-      .getByRole("textbox")
-      .fill(userPhoneNo);
-    await page.getByText("Save").click();
+    await createClientsPage.formName.fill(fullName);
+    await createClientsPage.formEmail.fill(userEmail);
+    await createClientsPage.formPhoneNo.fill(userPhoneNo);
+    await createClientsPage.save();
 
-    const element = page.locator(
-      "#app > div > div.clients > div:nth-last-child(1)"
+    await expect(page, "Check that the url is correct").toHaveURL(
+      /.*\/clients/
     );
+
+    await viewClientsPage.goto();
+
+    const element = viewClientsPage.lastItem;
     await expect(element).toContainText(fullName);
     await expect(element).toContainText(userEmail);
     await expect(element).toContainText(userPhoneNo);
 
-    // Click on "..."
-    await element.locator("div.action").click();
-
     // Delete new entry
-    const deleteElement = element.getByText("Delete");
-    await deleteElement.click();
+    await viewClientsPage.deleteLastItem();
   });
 });
